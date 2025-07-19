38aacfe70a097962bf80cbf762f7a370ed53736b
Fernando9200 <fernando91.sosa@hotmail.com>
2025-07-19 16:57:15 -0300

    Last files

--- src/components/RemindersPage.tsx
+++ src/components/RemindersPage.tsx
@@ -128,7 +128,7 @@ const RemindersPage: React.FC = () => {
         <aside className="w-64 bg-white border-r border-gray-200 flex-col hidden sm:flex">
           <div className="p-6 border-b border-gray-200">
             <h1 className="font-semibold text-xl text-gray-900">Lembretes</h1>
-            <p className="text-sm text-gray-500">Suas tarefas em um só lugar.</p>
+            <p className="text-sm text-gray-500">Suas tarefas em um só lugar</p>
           </div>
           <div className="flex-1 overflow-y-auto">
             <div className="p-4">
